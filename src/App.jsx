d18c535218294711ac7e6452e1d2bc847e4e111c
Sadiq Salau <sadiqsalau888@gmail.com>
2024-10-24 15:12:59 +0100

    Updated Mini-App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useCallback } from "react";
+import Icon from "./assets/images/icon.png?format=webp&w=256";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleButtonClick = useCallback(() => {
+    window.Telegram.WebApp?.openTelegramLink("https://t.me/purrfect_drop");
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex flex-col p-4 min-h-dvh">
+      <div className="flex flex-col w-full max-w-xs gap-3 m-auto">
+        <img src={Icon} className="mx-auto w-28 h-28" />
+        <h1 className="text-3xl text-center font-turret-road">Purrfect</h1>
+        <button
+          onClick={handleButtonClick}
+          className="px-4 py-2 text-white bg-orange-500 rounded-lg hover:bg-orange-600"
+        >
+          Open Channel
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { imagetools } from "vite-imagetools";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "~@fontsource": "@fontsource",
+    },
+  },
+  plugins: [react(), imagetools()],
+});
