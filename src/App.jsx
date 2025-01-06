d92c8b4451a4cf4419abc82250c7538e28db8e62
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-06 14:57:34 +0100

    Added User's username and Chat Group Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useCallback, useEffect } from "react";
 
 import Icon from "./assets/images/icon.png?format=webp&w=256";
+import copy from "copy-to-clipboard";
 
 function App() {
-  const handleButtonClick = useCallback(() => {
-    window.Telegram?.WebApp?.openTelegramLink(
-      "https://t.me/purrfect_community"
-    );
-  }, []);
+  /** Open Telegram Link */
+  const openTelegramLink = useCallback(
+    (link) => window.Telegram?.WebApp?.openTelegramLink(link),
+    []
+  );
+
+  /** Open Channel */
+  const openChannel = useCallback(() => {
+    openTelegramLink("https://t.me/purrfect_community");
+  }, [openTelegramLink]);
+
+  /** Open Chat */
+  const openChat = useCallback(() => {
+    openTelegramLink("https://t.me/purrfect_group_chat");
+  }, [openTelegramLink]);
+
+  /** Copy Username */
+  const copyUsername = useCallback(
+    () =>
+      copy(
+        `@${
+          window.Telegram?.WebApp?.initDataUnsafe?.user?.username ||
+          "purrfect_little_bot"
+        }`
+      ),
+    []
+  );
 
   /** Configure Telegram App */
   useEffect(() => {
@@ -22,13 +45,29 @@ function App() {
         <h1 className="text-3xl leading-none text-center text-orange-500 font-turret-road">
           Purrfect
         </h1>
-        <p className="text-center">Your purrfect little bot</p>
-        <button
-          onClick={handleButtonClick}
-          className="px-4 py-2 text-white bg-orange-500 rounded-lg hover:bg-orange-600"
+        <p
+          className="mx-auto text-center text-orange-500 truncate"
+          onClick={copyUsername}
         >
-          Open Channel
-        </button>
+          @
+          {window.Telegram?.WebApp?.initDataUnsafe?.user?.username ||
+            "purrfect_little_bot"}
+        </p>
+        <p className="text-center">Your purrfect little bot</p>
+        <div className="grid grid-cols-2 gap-2">
+          <button
+            onClick={openChannel}
+            className="px-4 py-2 text-white bg-purple-500 rounded-full hover:bg-purple-600"
+          >
+            Channel
+          </button>
+          <button
+            onClick={openChat}
+            className="px-4 py-2 text-white rounded-full bg-lime-500 hover:bg-lime-600"
+          >
+            Chat
+          </button>
+        </div>
       </div>
     </div>
   );
