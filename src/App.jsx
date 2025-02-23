06d66234331ba08304058d9a6b0771c76c3d4631
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-23 08:28:12 +0100

    Added Channel and Group Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import {} from "react-icons/hi2";
+
 import copy from "copy-to-clipboard";
+import { IoChatbubblesOutline, IoFlashOutline } from "react-icons/io5";
 import { useCallback, useEffect } from "react";
+
 import Icon from "./assets/images/icon-unwrapped-cropped.png?format=webp&h=256";
+import { cn } from "./lib/utils";
 
 function App() {
   /** Copy Username */
   const copyUsername = useCallback(
     () => copy(`@${window.Telegram?.WebApp?.initDataUnsafe?.user?.username}`),
+    []
+  );
+
+  /** Copy User ID */
+  const copyUserId = useCallback(
+    () => copy(window.Telegram?.WebApp?.initDataUnsafe?.user?.id),
     []
   );
 
@@ -15,9 +26,14 @@ function App() {
     []
   );
 
-  /** Open Dev Contact */
-  const openDevContact = useCallback(() => {
-    openTelegramLink("https://t.me/sadiqsalau");
+  /** Open Channel */
+  const openChannel = useCallback(() => {
+    openTelegramLink("https://t.me/purrfect_community");
+  }, [openTelegramLink]);
+
+  /** Open Chat */
+  const openChat = useCallback(() => {
+    openTelegramLink("https://t.me/purrfect_community_chat");
   }, [openTelegramLink]);
 
   /** Configure Telegram App */
@@ -28,7 +44,7 @@ function App() {
 
   return (
     <div className="flex flex-col p-4 min-h-dvh">
-      <div className="flex flex-col w-full max-w-xs gap-4 m-auto">
+      <div className="flex flex-col w-full max-w-xs gap-2 m-auto">
         {/* Logo */}
         <img src={Icon} className="h-24 mx-auto" />
 
@@ -45,31 +61,56 @@ function App() {
               src={window.Telegram?.WebApp?.initDataUnsafe?.user?.["photo_url"]}
             />
 
-            <div className="flex flex-col min-w-0 min-h-0 text-sm grow">
+            <div className="flex flex-col min-w-0 min-h-0 text-sm grow pr-2">
               {/* First and Last Name */}
               <p className="font-bold text-purple-500 truncate">
                 {window.Telegram?.WebApp?.initDataUnsafe?.user?.[
                   "first_name"
-                ] || "Purrfect"}{" "}
+                ] || "Telegram"}{" "}
                 {window.Telegram?.WebApp?.initDataUnsafe?.user?.["last_name"] ||
-                  "Bot"}
+                  "User"}
               </p>
 
               {/* Username */}
               {window.Telegram?.WebApp?.initDataUnsafe?.user?.username ? (
-                <p
-                  onClick={copyUsername}
-                  className="font-bold text-yellow-500 truncate"
-                >
+                <p onClick={copyUsername} className="text-yellow-500 truncate">
                   @{window.Telegram?.WebApp?.initDataUnsafe?.user?.username}
                 </p>
               ) : null}
 
               {/* User ID */}
-              <p className="font-bold truncate text-lime-500">
+              <p onClick={copyUserId} className="truncate text-lime-500">
                 ID: {window.Telegram?.WebApp?.initDataUnsafe?.user?.id}
               </p>
             </div>
+          </div>
+        </div>
+
+        {/* Channel & Chat */}
+        <div className="flex justify-center items-center text-sm">
+          <div className="grid grid-cols-2 gap-2">
+            <button
+              onClick={openChannel}
+              className={cn(
+                "px-4 py-2 rounded-full",
+                "flex items-center gap-2",
+                "bg-purple-300 text-black"
+              )}
+            >
+              <IoFlashOutline />
+              Channel
+            </button>
+            <button
+              onClick={openChat}
+              className={cn(
+                "px-4 py-2 rounded-full",
+                "flex items-center gap-2",
+                "bg-lime-300 text-black"
+              )}
+            >
+              <IoChatbubblesOutline />
+              Group
+            </button>
           </div>
         </div>
       </div>
